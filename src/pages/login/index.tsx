0ddbce993c16480301d0c6ec8aa79ba9d3736a63
NROSTARP <nrostarp0765@gmail.com>
2022-06-13 11:05:31 +0700

    update login, register, dashboard Page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,6 +10,15 @@ import { Grid,  } from '@react-md/utils'
 import styles from '../../styles/Home.module.css'
 import logo from '../../images/logo/matrix-robot-logo-ex.svg'
 
+
+import {
+  Form,
+  TextField,
+  Password
+} from "@react-md/form";
+
+import { Divider } from 'react-md'
+
 const Login: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -23,9 +32,32 @@ const Login: NextPage = () => {
         <Image src={logo} className={styles.logo} alt="Logo" />
 
         <div className='login'>
-        <LinkStyledButton href="/home">Home</LinkStyledButton>
-        <LinkStyledButton href="/home">Home</LinkStyledButton>
+
+          <h4 className={styles.text_center}>ลงชื่อเข้าใช้งาน</h4>
+
+        <Form className={styles.formlogin}>
+
+        <TextField
+            id="email-field"
+            label="Email or Username"
+            placeholder="Email or Username"
+          />
+
+            <Divider className={styles.Divider}></Divider>
+
+          <Password
+          id="password-field"
+          label="Enter Password"
+          placeholder="Enter Password">
+
+          </Password>
+
+        </Form>
+
+        <LinkStyledButton href="/admin" className={styles.btnregister}>Login</LinkStyledButton>
+
         </div>
+        <Divider></Divider>
         <Grid columns={2}>
           <LinkStyledButton href="/home">Home</LinkStyledButton>
           <LinkStyledButton href="/register">Register</LinkStyledButton>
